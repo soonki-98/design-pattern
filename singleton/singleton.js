2ab5a433a15bd463d73210ed9afd7f08438706a0
민순기 <skmin@rsupport.com>
2024-01-13 10:22:59 +0900

    sigleton pattern

---
+++ singleton/singleton.js
@@ -0,0 +1,22 @@
+"use strict";
+class Singleton {
+    constructor() {
+        if (Singleton.instance) {
+            console.warn("singleton class already instantiated");
+            throw new Error("에러");
+        }
+        Singleton.instance = this;
+        this.version = new Date();
+    }
+    static getInstance() {
+        if (!this.instance) {
+            this.instance = new Singleton();
+        }
+        return this.instance;
+    }
+}
+const s1 = Singleton.getInstance();
+console.log(s1);
+const s2 = Singleton.getInstance();
+console.log(s2);
+console.log(s1 === s2);
